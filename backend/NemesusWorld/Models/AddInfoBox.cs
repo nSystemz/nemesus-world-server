b26dd986894515ecb6a9f10d90c3e901ae9d63aa
Tim S <info@nsystemz.de>
2024-07-05 10:03:07 +0200

    AddInfoBox Anpassungen

---
+++ backend/NemesusWorld/Models/AddInfoBox.cs
@@ -0,0 +1,25 @@
+﻿using GTANetworkAPI;
+using NemesusWorld.Utils;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace NemesusWorld.Models
+{
+    class AddInfoBox
+    {
+        public int id { get; set; }
+        public Vector3 position { get; set; }
+        public GTANetworkAPI.TextLabel label { get; set; }
+        public String creator { get; set; }
+        public int created { get; set; }
+
+        public AddInfoBox()
+        {
+            id = 0;
+            creator = "n/A";
+            created = Helper.UnixTimestamp();
+        }
+    }
+}
